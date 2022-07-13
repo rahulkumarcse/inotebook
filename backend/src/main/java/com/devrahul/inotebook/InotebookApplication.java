228cffb413e7887450d71a643f3ae5e3160cfefc
Rahul Kumar <rahulcsofficial@gmail.com>
2022-07-14 00:26:39 +0530

    Backend converting to java spring framework

---
+++ backend/src/main/java/com/devrahul/inotebook/InotebookApplication.java
@@ -0,0 +1,13 @@
+package com.devrahul.inotebook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InotebookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InotebookApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/devrahul/inotebook/InotebookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devrahul.inotebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InotebookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
