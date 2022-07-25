0ef0ccdc226dae8ca38976da5c4b618df0800df8
Rahul Kumar <rahulcsofficial@gmail.com>
2022-07-25 23:59:13 +0530

    useLocation added with active status for links

--- src/components/About.js
+++ src/components/About.js
@@ -3,14 +3,10 @@ import { useContext } from 'react'
 import NoteContext from '../context/note/NoteContext'
 
 function About() {
-  const a = useContext(NoteContext)
-  useEffect( ()=>{
-    a.update()
-  },[])
   return (
     
     <div>
-      This is about {a.state.name} AND HE IS IN {a.state.class}
+      This is about page
     </div>
   )
 }
--- src/context/note/NoteState.js
+++ src/context/note/NoteState.js
@@ -2,22 +2,8 @@ import React, { useState } from "react";
 import NoteContext from "./NoteContext";
 
 const Notestate = (props) =>{
-    const s1 ={
-        'name' : 'rahul',
-        'class' :'18'
-    }
-    const update =()=>{
-       setTimeout( () =>{
-        setState({
-            'name' : 'Rahul Kumar',
-        'class' :'Boss'
-        })
-       }, 1000
-       )
-    }
-    const [state,setState] = useState(s1);
     return(
-        <NoteContext.Provider value={{state, update}}>
+        <NoteContext.Provider value={{}}>
                 {props.children}
         </NoteContext.Provider>
     );
