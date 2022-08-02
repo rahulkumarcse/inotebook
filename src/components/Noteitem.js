0e3f12c4238a5a08ca6e69bfdaa89402eef1f1d7
Rahul Kumar <rahulcsofficial@gmail.com>
2022-08-02 17:24:03 +0530

    Update notr click is working fine, needs to integrate update api

--- src/components/Noteitem.js
+++ src/components/Noteitem.js
@@ -1,12 +1,12 @@
-import React,{useContext} from 'react'
+import React,{useContext, useRef} from 'react'
 import NoteContext from '../context/note/NoteContext';
 
+
 function Noteitem(props) {
-    const { note } = props;
+    const { note , updateNote } = props;
     const context = useContext(NoteContext);
     const { deleteNote } = context
-
-
+ 
     return (
         <div className='col-md-3'>
             <div className="card my-3">
@@ -14,7 +14,7 @@ function Noteitem(props) {
                     <div className='d-flex align-items-center'>
                         <h5 className="card-title"> {note.title}</h5>
                         <i className="far  fa-solid fa-trash mx-3 btn"  onClick={()=>{deleteNote(note.id)}}/>
-                        <i className="fa-solid fa-pen-to-square mx-3"></i>
+                        <i className=" fa-solid fa-pen-to-square mx-3 btn" onClick={()=>{updateNote(note)}}></i>
                     </div>
 
                     <p className="card-text"> {note.description}</p>
